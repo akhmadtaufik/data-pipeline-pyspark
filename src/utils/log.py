f60b346c98efca5dcd667b105870e63ded3f31da
Akhmad Taufik <akhmadtaufik1703@gmail.com>
2025-03-16 22:42:37 +0700

    feat(logging): add ETLLogger for tracking ETL process - implement logging to PostgreSQL using pandas and SQLAlchemy - add method to fetch last successful ETL run for a given process and table

---
+++ src/utils/log.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from datetime import datetime
+from typing import Any, Optional
+from src.utils.config import (
+    POSTGRES_HOST,
+    POSTGRES_PORT,
+    POSTGRES_USER,
+    POSTGRES_PASSWORD,
+    DB_LOG,
+)
+
+
+class ETLLogger:
+    def __init__(self):
+        self.engine = self._create_engine()
+
+    def _create_engine(self):
+        engine = create_engine(
+            f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+            f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{DB_LOG}"
+        )
+
+        return engine
+
+    def log(self, log_msg: dict[str, Any]):
+        try:
+            df_log = pd.DataFrame([log_msg])
+            df_log.to_sql(
+                name="startup_etl_log",
+                con=self.engine,
+                if_exists="append",
+                index=False,
+            )
+
+        except Exception as e:
+            print(f"Gagal menyimpan log: {str(e)}")
+
+    def info(self, log_msg: dict[str, Any]):
+        self.log(log_msg)
+
+    def get_last_run(
+        self, process_name: str, table_name: str
+    ) -> Optional[datetime]:
+        query = f"""
+        SELECT MAX(etl_date)
+        FROM startup_etl_log
+        WHERE process = '{process_name}'
+          AND table_name = '{table_name}'
+          AND status = 'success'
+        """
+
+        try:
+            last_run = pd.read_sql_query(query, self.engine).iloc[0, 0]  # type: ignore
+
+            return last_run if last_run else datetime(1900, 1, 1)  # type: ignore
+
+        except Exception as e:
+            print(f"Gagal membaca log: {str(e)}")
+            return datetime(1900, 1, 1)
