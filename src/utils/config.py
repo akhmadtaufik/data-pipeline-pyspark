ac5a5ec1a653e0e6e4505c5e9e5e08b7ab1002cc
Akhmad Taufik <akhmadtaufik1703@gmail.com>
2025-03-17 13:06:22 +0700

    feat(config): add MinIO credentials - add MINIO_ENDPOINT, MINIO_PROFILING_BUCKET_NAME, MINIO_BUCKET_NAME - add MINIO_ACCESS_KEY and MINIO_SECRET_KEY - improve environment configuration management

--- src/utils/config.py
+++ src/utils/config.py
@@ -3,15 +3,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Kredensial PostgreSQL
+# PostgreSQL Credentials
 POSTGRES_HOST = os.getenv("POSTGRES_HOST")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 
+# Database Name
 DB_SOURCE = os.getenv("DB_SOURCE")
 DB_LOG = os.getenv("DB_LOG")
 DB_STAGING = os.getenv("DB_STAGING")
 
 # Endpoint API
 MILESTONES_API = os.getenv("MILESTONES_API")
+
+# MinIO
+MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT")
+MINIO_PROFILING_BUCKET_NAME = os.getenv("MINIO_PROFILING_BUCKET_NAME")
+MINIO_BUCKET_NAME = os.getenv("MINIO_BUCKET_NAME")
+MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY")
+MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY")
