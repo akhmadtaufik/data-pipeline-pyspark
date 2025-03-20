fd8a2fa6d3039210b0faa9bff6598e799e180e95
Akhmad Taufik <akhmadtaufik1703@gmail.com>
2025-03-20 13:52:10 +0700

    refactor(extract): enhance CSV extraction with logging and error handling - added log_operation to track extraction status - extracted table name from file path for better logging - implemented exception handling for robust error management

--- src/profiling/extract/extract_csv.py
+++ src/profiling/extract/extract_csv.py
@@ -1,21 +1,44 @@
 from pyspark.sql import DataFrame
 from src.utils.spark_session import init_spark_session
+from src.utils.log_message import log_operation
 
 
-def extract_csv(file_path: str) -> DataFrame:
+def extract_csv(file_path: str):
     """
-    Extract a CSV file into a DataFrame using a SparkSession.
+    Extracts data from a CSV file into a DataFrame and logs the operation.
 
-    This function initializes a SparkSession and reads a CSV file from the
-    specified file path into a DataFrame. The CSV file is expected to have a
-    header row, and the schema is inferred automatically.
+    This function initializes a SparkSession, reads a CSV file into a DataFrame,
+    and logs the success or failure of the extraction process. The log includes
+    details such as the step, status, source, and table name.
 
     Args:
-        file_path (str): The path to the CSV file to be read.
+        file_path (str): The path to the CSV file to be extracted.
 
     Returns:
-        DataFrame: A DataFrame containing the data from the CSV file.
+        DataFrame | None: The DataFrame containing the extracted data if successful,
+        otherwise None if an error occurs during extraction.
     """
     spark = init_spark_session()
+    table_name = file_path.split("/")[-1].split(".")[0]
 
-    return spark.read.csv(file_path, header=True, inferSchema=True)
+    try:
+        df = spark.read.csv(file_path, header=True, inferSchema=True)
+
+        log_operation(
+            step="extract",
+            status="success",
+            source="csv",
+            table_name=table_name,
+            message=f"Berhasil ekstrak {table_name}",
+        )
+
+        return df
+
+    except Exception as e:
+        log_operation(
+            step="extract",
+            status="failed",
+            source="csv",
+            table_name=table_name,
+            error_msg=str(e),
+        )
