1b905a0ee0add86c485b0393237e08d52fe1bdf3
Akhmad Taufik <akhmadtaufik1703@gmail.com>
2025-04-15 14:40:04 +0700

    feat(utils): add reusable psycopg2 connection utility for database access
    - Add  function for establishing PostgreSQL connections
    - Centralize connection logic to reduce duplication in data loading and extraction

---
+++ src/utils/psycopg2_connection.py
@@ -0,0 +1,28 @@
+import psycopg2
+from src.utils.config import (
+    POSTGRES_HOST,
+    POSTGRES_PORT,
+    POSTGRES_USER,
+    POSTGRES_PASSWORD,
+)
+
+
+def read_db(db_name: str):
+    """
+    Establishes a connection to a PostgreSQL database using the provided database name.
+
+    Args:
+        db_name (str): The name of the database to connect to.
+
+    Returns:
+        connection: A psycopg2 connection object to the specified PostgreSQL database.
+    """
+    conn = psycopg2.connect(
+        host=POSTGRES_HOST,
+        port=POSTGRES_PORT,
+        dbname=db_name,
+        user=POSTGRES_USER,
+        password=POSTGRES_PASSWORD,
+    )
+
+    return conn
